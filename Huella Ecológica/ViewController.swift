e5eae7ab84de1c707c192ccfd737748ae3f72745
Ricardo <ricardoharo@outlook.com>
2016-07-28 16:02:00 -0500

    Initial Commit

---
+++ Huella Ecológica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Huella Ecológica
+//
+//  Created by Ricardo Rodriguez Haro on 7/28/16.
+//  Copyright © 2016 Ricardo Rodriguez Haro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Huella EcológicaTests/Huella_Ecolo_gicaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Huella_Ecolo_gicaTests.swift
+//  Huella EcológicaTests
+//
+//  Created by Ricardo Rodriguez Haro on 7/28/16.
+//  Copyright © 2016 Ricardo Rodriguez Haro. All rights reserved.
+//
+
+import XCTest
+@testable import Huella_Ecolo_gica
+
+class Huella_Ecolo_gicaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
